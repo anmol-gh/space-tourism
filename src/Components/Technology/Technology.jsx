abe61a8fd30149549af82760873116c9dd04b045
noob-anmol <anmolghai50@gmail.com>
2024-06-29 01:22:27 +0530

    complete tech module

--- src/Components/Technology/Technology.jsx
+++ src/Components/Technology/Technology.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React from "react";
+import "./Technology.css";
+import { useState } from "react";
+import techData from "./TechnologyData.js";
 
 const Technology = () => {
-  return (
-    <div>Technology</div>
-  )
-}
+	const [tech, setTech] = useState("Launch");
+	return (
+		<div className='tech-container'>
+			<p className='tech-para'>
+				<span className='tech-para-span'> 03</span> SPACE LAUNCH 101
+			</p>
+			<div className='tech-card-container'>
+				<div className='tech-card tech-deets'>
+					<div className='tech-btns'>
+						<button
+							className='tech-btn'
+							onClick={() => {
+								setTech("Launch");
+							}}
+						>
+							1
+						</button>
+						<button
+							className='tech-btn'
+							onClick={() => {
+								setTech("Spaceport");
+							}}
+						>
+							2
+						</button>
+						<button
+							className='tech-btn'
+							onClick={() => {
+								setTech("Capsule");
+							}}
+						>
+							3
+						</button>
+					</div>
+					<div className='tech-card-details'>
+						<h3 className='tech-title'> {{ techData }.techData[tech].title}</h3>
+						<h1 className='tech-name'>{{ techData }.techData[tech].name}</h1>
+						<p className='tech-desc'>
+							{{ techData }.techData[tech].description}
+						</p>
+					</div>
+				</div>
+				<div className='tech-card'>
+					<img
+						className='tech-card-img'
+						src={{ techData }.techData[tech].image}
+						alt={"Image of " + tech}
+					/>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-export default Technology
+export default Technology;
