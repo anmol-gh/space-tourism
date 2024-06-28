0691346cb6e099b30f7adc45aa2fbec3e070ce1c
noob-anmol <anmolghai50@gmail.com>
2024-06-29 01:35:33 +0530

    added background images

--- src/Components/CheckURL.js
+++ src/Components/CheckURL.js
@@ -6,7 +6,9 @@ const checkURL = () => {
     const technology = document.getElementById("technology");
     const elements = [home, destination, crew, technology];
 
+    const root = document.getElementById("root");
     if (url.includes("destination")) {
+        root.style.backgroundImage = "url('https://raw.githubusercontent.com/anmol-gh/space-tourism/master/src/Assets/destination/background-destination-desktop.jpg')";
         elements[1].style.borderBottom = "2px solid #fff";
         elements[0].style.borderBottom = "none";
         elements[2].style.borderBottom = "none";
@@ -17,16 +19,23 @@ const checkURL = () => {
         elements[0].style.borderBottom = "none";
         elements[1].style.borderBottom = "none";
         elements[3].style.borderBottom = "none";
+        root.style.backgroundImage = "url('https://raw.githubusercontent.com/anmol-gh/space-tourism/master/src/Assets/crew/background-crew-desktop.jpg')";
+
     } else if (url.includes("technology")) {
         elements[3].style.borderBottom = "2px solid #fff";
         elements[0].style.borderBottom = "none";
         elements[1].style.borderBottom = "none";
         elements[2].style.borderBottom = "none";
+        // root.style.backgroundImage
+        root.style.backgroundImage = "url('https://raw.githubusercontent.com/anmol-gh/space-tourism/master/src/Assets/technology/background-technology-desktop.jpg')";
+
     } else {
         elements[0].style.borderBottom = "2px solid #fff";
         elements[1].style.borderBottom = "none";
         elements[2].style.borderBottom = "none";
         elements[3].style.borderBottom = "none";
+        root.style.backgroundImage = "url('https://raw.githubusercontent.com/anmol-gh/space-tourism/master/src/Assets/background-home-desktop.jpg')";
+
     }
 
 };
