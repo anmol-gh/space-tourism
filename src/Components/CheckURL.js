6ba3231c627f553c4093a90b0467d629aeeeb7f4
noob-anmol <anmolghai50@gmail.com>
2024-06-30 22:30:19 +0530

    tablet version of home, crew, destination

--- src/Components/CheckURL.js
+++ src/Components/CheckURL.js
@@ -10,30 +10,30 @@ const checkURL = () => {
     if (url.includes("destination")) {
         root.style.backgroundImage = "url('https://raw.githubusercontent.com/anmol-gh/space-tourism/master/src/Assets/destination/background-destination-desktop.jpg')";
         elements[1].style.borderBottom = "2px solid #fff";
-        elements[0].style.borderBottom = "none";
-        elements[2].style.borderBottom = "none";
-        elements[3].style.borderBottom = "none";
+        elements[0].style.borderBottom = "";
+        elements[2].style.borderBottom = "";
+        elements[3].style.borderBottom = "";
     }
     else if (url.includes("crew")) {
         elements[2].style.borderBottom = "2px solid #fff";
-        elements[0].style.borderBottom = "none";
-        elements[1].style.borderBottom = "none";
-        elements[3].style.borderBottom = "none";
+        elements[0].style.borderBottom = "";
+        elements[1].style.borderBottom = "";
+        elements[3].style.borderBottom = "";
         root.style.backgroundImage = "url('https://raw.githubusercontent.com/anmol-gh/space-tourism/master/src/Assets/crew/background-crew-desktop.jpg')";
 
     } else if (url.includes("technology")) {
         elements[3].style.borderBottom = "2px solid #fff";
-        elements[0].style.borderBottom = "none";
-        elements[1].style.borderBottom = "none";
-        elements[2].style.borderBottom = "none";
+        elements[0].style.borderBottom = "";
+        elements[1].style.borderBottom = "";
+        elements[2].style.borderBottom = "";
         // root.style.backgroundImage
         root.style.backgroundImage = "url('https://raw.githubusercontent.com/anmol-gh/space-tourism/master/src/Assets/technology/background-technology-desktop.jpg')";
 
     } else {
         elements[0].style.borderBottom = "2px solid #fff";
-        elements[1].style.borderBottom = "none";
-        elements[2].style.borderBottom = "none";
-        elements[3].style.borderBottom = "none";
+        elements[1].style.borderBottom = "";
+        elements[2].style.borderBottom = "";
+        elements[3].style.borderBottom = "";
         root.style.backgroundImage = "url('https://raw.githubusercontent.com/anmol-gh/space-tourism/master/src/Assets/background-home-desktop.jpg')";
 
     }
